b9cb632059c97596d2867c91c5b13b0d83bb8940
Zoe Cohen <zoe.cohen@sap.com>
2021-10-28 08:51:23 +0300

    change in CDP.ts for US5 datacenter

--- CDP.ts
+++ CDP.ts
@@ -8,7 +8,7 @@ import {wrap} from "./ts-rest-client";
 import {RequestOptions, sendRequest} from "./sendRequest";
 import {WithHeaders} from "./ts-rest-client/interfaces/EntityApiTypes";
 
-export type DataCenter = 'eu5' | `il1`;
+export type DataCenter = 'eu5' | `us5` | `il1`;
 type StagingEnvs = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 export type Env<n extends StagingEnvs = StagingEnvs> = 'prod' | `st${n}`;
 export const availableEnvs: Record<DataCenter, Env[]> = {
@@ -157,15 +157,10 @@ export class CDP {
         return paths.every(p => !!apiAcl[`${this.options.rootPath}/${p}`]);
     }
 
-    // private getDomainDc({dataCenter, env} = this.options) {
-    //     if (dataCenter == 'eu5' && env == 'prod')
-    //         return 'eu5';
-    //
-    //     const dc = dataCenter == 'il1' ? 'il1-cdp' : 'eu5';
-    //     return `${dc}-${env}`;
-    // }
-
     private getDomainDc({dataCenter, env} = this.options) {
+        if (dataCenter != 'il1' && env == 'prod')
+            return dataCenter;
+
         const dc = dataCenter.includes('il1') ? 'il1-cdp' : dataCenter;
         return `${dc}-${env}`;
     }
