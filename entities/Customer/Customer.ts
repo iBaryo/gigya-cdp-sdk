56536d8146efc6f50d8db1d5c4f4ea2f2f4287da
Zoe Cohen <zoe.cohen@sap.com>
2022-02-10 12:47:39 +0200

    00000_edit directories

---
+++ entities/Customer/Customer.ts
@@ -0,0 +1,40 @@
+import {WithViewId} from "../View";
+import {Id, ISODateTimeString, WithMetaData} from "../common";
+import {ProfileFieldName} from "../common/Field";
+import {ActivityIndicatorName} from "../ActivityIndicator";
+import {CalculationMethod} from "../ActivityIndicator/CalculationMethod";
+import {SegmentName, SegmentValue} from "../Segment";
+import {Purpose, PurposeId, PurposeStatus} from "../Purpose";
+
+export interface Customer extends WithViewId, WithMetaData {
+    _id: Id;
+    lastSeen: ISODateTimeString;
+    firstSeen: ISODateTimeString;
+
+    attributes: Record<ProfileFieldName, any>;
+    activityIndicators: Array<{
+        name: ActivityIndicatorName;
+        updated: WithMetaData['updated'];
+    } & { [K in CalculationMethod['method']]: number | string }>;
+
+    segments: Array<{
+        name: SegmentName;
+        value: SegmentValue;
+        updated: WithMetaData['updated'];
+    }>;
+
+    privacy: {
+        purposes: Array<{
+            purposeId: PurposeId;
+            externalId: Purpose['externalId'];
+            status: PurposeStatus;
+            date: ISODateTimeString;
+        }>;
+
+        subscriptions: Array<{
+            channelType: string;
+            status: PurposeStatus;
+            date: ISODateTimeString;
+        }>;
+    }
+}
